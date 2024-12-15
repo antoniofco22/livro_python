32ed7779d7b5837d01a54a2a642ee9624ccd77e2
antonio francisco alves dos santos <103600179+antoniofco22@users.noreply.github.com>
2024-12-15 14:00:47 -0300

    Update main.py
    add cors

--- main.py
+++ main.py
@@ -6,12 +6,26 @@ from database import engine, Base, SessaoLocal
 from models import Livro, Estudante, Emprestimo
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Adicionando o middleware de CORS
+origins = [
+    "http://localhost:3000",  # Exemplo para ambiente local
+    "https://antoniofco22.github.io",  # Substitua pelo domínio do seu front-end
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Permitir essas origens
+    allow_credentials=True,  # Permitir cookies e autenticação
+    allow_methods=["*"],  # Permitir todos os métodos
+    allow_headers=["*"],  # Permitir todos os cabeçalhos
+)
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-#sessão do banco de dados
+# Sessão do banco de dados
 def obter_sessao_db():
     db = SessaoLocal()
     try:
@@ -19,30 +33,30 @@ def obter_sessao_db():
     finally:
         db.close()
 
-# modelo login
+# Modelo de login
 class DadosLogin(BaseModel):
     usuario: str
     senha: str
 
-#rota login admin
+# Rota de login para admin
 @app.post("/login/")
 async def login(dados: DadosLogin):
     if autenticar_usuario(dados.usuario, dados.senha):
         token = criar_token_acesso({"sub": dados.usuario})
         return {"token_de_acesso": token}
-    raise HTTPException(status_code=403, detail="usuario ou senha errado")
+    raise HTTPException(status_code=403, detail="Usuário ou senha errados")
 
-#autenticacao
+# Autenticação
 async def obter_usuario_atual(token: str = Depends(oauth2_scheme)):
     credenciais = verificar_token(token)
     if not credenciais:
-        raise HTTPException(status_code=403, detail="token invalido")
+        raise HTTPException(status_code=403, detail="Token inválido")
     return credenciais
 
-#emprestar
+# Empréstimo de livro
 @app.post("/emprestar/")
 async def emprestar_livro(id_livro: int, id_estudante: int, db: Session = Depends(obter_sessao_db)):
     emprestimo = Emprestimo(book_id=id_livro, student_id=id_estudante, borrow_date=date.today())
     db.add(emprestimo)
     db.commit()
-    return {"mensagem": "emprestimo feito!"}
+    return {"mensagem": "Empréstimo feito com sucesso!"}
